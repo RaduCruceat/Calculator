a532adf959ca72b3e0cb470846851f32b534953d
RaduCruceat <radu.cruceat@gmail.com>
2024-05-05 00:33:22 +0300

    design

--- Gamble/Gamble.py
+++ Gamble/Gamble.py
@@ -18,19 +18,15 @@ def btn_equal():
 
 expression = ""
 
-# Creating a basic window
 window = tk.Tk()
 window.title("Calculator")
-window.configure(bg='grey')  # Set background color to grey
+window.configure(bg='grey')  
 
-# StringVar() is used to get the instance of input field
 input_text = tk.StringVar()
 
-# Creating the entry field
-entry_field = tk.Entry(window, textvariable=input_text, font=('Arial', 18, 'bold'), bd=20, insertwidth=4, width=14, bg="green", fg="white", justify='right')  # Set text color to white and background color to green
+entry_field = tk.Entry(window, textvariable=input_text, font=('Arial', 18, 'bold'), bd=20, insertwidth=4, width=14, bg="green", fg="black", justify='right')  
 entry_field.grid(columnspan=4)
 
-# Buttons
 buttons = [
     ('7', '8', '9', '/'),
     ('4', '5', '6', '*'),
@@ -38,15 +34,13 @@ buttons = [
     ('C', '0', '=', '+')
 ]
 
-# Adding buttons to the window
 row = 1
 for button_row in buttons:
     col = 0
     for button_text in button_row:
-        tk.Button(window, text=button_text, padx=20, pady=20, font=('Arial', 18, 'bold'), fg="white", bg="grey",  # Set text color to white and background color to grey
+        tk.Button(window, text=button_text, padx=20, pady=20, font=('Arial', 18, 'bold'), fg="black", bg="grey",  
                   command=lambda button_text=button_text: btn_click(button_text) if button_text != '=' else btn_equal() if button_text == '=' else btn_clear()).grid(row=row, column=col)
         col += 1
     row += 1
 
-# Running the application
 window.mainloop()
